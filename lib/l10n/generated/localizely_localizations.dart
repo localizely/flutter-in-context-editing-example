282af4cc9e3915d8e6b9f8df245b69d3023c5f91
Zoran Luledzija <zoran.luledzija@outlook.com>
2025-03-05 10:20:52 +0100

    Update localizely_sdk dependency

--- lib/l10n/generated/localizely_localizations.dart
+++ lib/l10n/generated/localizely_localizations.dart
@@ -4,48 +4,49 @@ import 'package:localizely_sdk/localizely_sdk.dart';
 
 import 'app_localizations.dart';
 
+// ignore_for_file: type=lint
+
 class LocalizelyLocalizations extends AppLocalizations {
-	final AppLocalizations _fallback;
+  final AppLocalizations _fallback;
 
-	LocalizelyLocalizations(String locale, AppLocalizations fallback) : _fallback = fallback, super(locale);
+  LocalizelyLocalizations(String locale, AppLocalizations fallback) : _fallback = fallback, super(locale);
 
-	static const LocalizationsDelegate<AppLocalizations> delegate = _LocalizelyLocalizationsDelegate();
+  static const LocalizationsDelegate<AppLocalizations> delegate = _LocalizelyLocalizationsDelegate();
 
-	static const List<LocalizationsDelegate<dynamic>> localizationsDelegates = <LocalizationsDelegate<dynamic>>[
-		delegate,
-		GlobalMaterialLocalizations.delegate,
-		GlobalCupertinoLocalizations.delegate,
-		GlobalWidgetsLocalizations.delegate,
-	];
+  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates = <LocalizationsDelegate<dynamic>>[
+    delegate,
+    GlobalMaterialLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+  ];
 
-	static const List<Locale> supportedLocales = AppLocalizations.supportedLocales;
+  static const List<Locale> supportedLocales = AppLocalizations.supportedLocales;
 
-	@override
-	String get appTitle => LocalizelyGenL10n.getText(localeName, 'appTitle') ?? _fallback.appTitle;
+  @override
+  String get appTitle => LocalizelyGenL10n.getText(localeName, 'appTitle') ?? _fallback.appTitle;
 
-	@override
-	String get commonNotYetImplemented => LocalizelyGenL10n.getText(localeName, 'commonNotYetImplemented') ?? _fallback.commonNotYetImplemented;
+  @override
+  String get commonNotYetImplemented => LocalizelyGenL10n.getText(localeName, 'commonNotYetImplemented') ?? _fallback.commonNotYetImplemented;
 
-	@override
-	String get pageHomeTitle => LocalizelyGenL10n.getText(localeName, 'pageHomeTitle') ?? _fallback.pageHomeTitle;
+  @override
+  String get pageHomeTitle => LocalizelyGenL10n.getText(localeName, 'pageHomeTitle') ?? _fallback.pageHomeTitle;
 
-	@override
-	String get pageHomeButtonStart => LocalizelyGenL10n.getText(localeName, 'pageHomeButtonStart') ?? _fallback.pageHomeButtonStart;
-
+  @override
+  String get pageHomeButtonStart => LocalizelyGenL10n.getText(localeName, 'pageHomeButtonStart') ?? _fallback.pageHomeButtonStart;
 }
 
 class _LocalizelyLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
-	const _LocalizelyLocalizationsDelegate();
+  const _LocalizelyLocalizationsDelegate();
 
-	@override
-	Future<AppLocalizations> load(Locale locale) => AppLocalizations.delegate.load(locale).then((fallback) {
-		LocalizelyGenL10n.setCurrentLocale(locale.toString());
-		return LocalizelyLocalizations(locale.toString(), fallback);
-	});
+  @override
+  Future<AppLocalizations> load(Locale locale) => AppLocalizations.delegate.load(locale).then((fallback) {
+    LocalizelyGenL10n.setCurrentLocale(locale.toString());
+    return LocalizelyLocalizations(locale.toString(), fallback);
+  });
 
-	@override
-	bool isSupported(Locale locale) => AppLocalizations.delegate.isSupported(locale);
+  @override
+  bool isSupported(Locale locale) => AppLocalizations.delegate.isSupported(locale);
 
-	@override
-	bool shouldReload(_LocalizelyLocalizationsDelegate old) => false;
+  @override
+  bool shouldReload(_LocalizelyLocalizationsDelegate old) => false;
 }
