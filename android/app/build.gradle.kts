f208145079e28bc6bbdb08c235e22e7ba0476d88
Zoran Luledzija <zoran.luledzija@outlook.com>
2025-09-15 15:00:33 +0200

    Update codebase with the latest changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.flutter_in_context_editing_example"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973" // flutter.ndkVersion
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
